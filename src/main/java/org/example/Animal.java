8367d6810af5a197af980d96ef0b7fda6906acd9
ursulakohler <kohlerursula22@gmail.com>
2024-07-09 11:24:45 +0200

    add new record Animal

--- src/main/java/org/example/Animal.java
+++ src/main/java/org/example/Animal.java
@@ -1,2 +1,12 @@
-package org.example;public record Animal() {
+package org.example;
+
+//Erstellen Sie einen Java-Datensatz "Tier", der Eigenschaften für ID, Name, Art und Alter haben sollte. Wählen Sie geeignete Feldnamen.
+//
+//Machen Sie einen Commit.
+
+public record Animal(
+        int id,
+        String name,
+        int age) {
+
 }
