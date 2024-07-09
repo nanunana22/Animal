63aaf1a402e90e49d0540726733f2d295e297e99
ursulakohler <kohlerursula22@gmail.com>
2024-07-09 11:57:15 +0200

    add main

--- src/main/java/org/example/Animal.java
+++ src/main/java/org/example/Animal.java
@@ -1,13 +1,25 @@
 package org.example;
 
 //Erstellen Sie einen Java-Datensatz "Tier", der Eigenschaften für ID, Name, Art und Alter haben sollte. Wählen Sie geeignete Feldnamen.
-//
-//Machen Sie einen Commit.
+//Erstellen Sie in einer Hauptmethode mehrere Animals.
+
+//Erstellen Sie in einer Hauptmethode mehrere Animals.
+//Rufen Sie die automatisch generierten Methoden im Record auf und prüfen Sie,
+// ob sie das tun, was Sie erwarten (equals, toString).
+
 
 public record Animal(
         int id,
         String name,
         String species,
         int age) {
-
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "id: " + id +
+                " name: " + name  +
+                " species: " + species +
+                " age: " + age +
+                '}';
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,21 @@
 package org.example;
 
+public class Main {
+    public static void main(String[] args) {
+        Animal animal = new Animal(1,"Maysie","Katze",2);
+        System.out.println(animal);
 
+
+        Animal animal2 = new Animal(2,"Sammy","Katze",2);
+        System.out.println(animal2);
+
+        System.out.println(animal.equals(animal2));
+
+
+        System.out.println(animal.toString());
+
+
+    }
+}
 
 
